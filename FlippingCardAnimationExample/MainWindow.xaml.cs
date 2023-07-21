358a600ce4f36cb9eb5fc752218c014dd43dcd83
lukewire <xlwpdldhk@gmail.com>
2023-07-21 18:16:29 +0900

    초기 커밋.

---
+++ FlippingCardAnimationExample/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Animation;
+using System.Windows.Media;
+
+namespace FlippingCardAnimationExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent ();
+        }
+        private bool isFlip = false;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            isFlip = !isFlip;
+
+            if (isFlip)
+            {
+                FrontAnimation ();
+                return;
+            }
+            BackAnimation ();
+        }
+        public void FrontAnimation()
+        {
+            backCard.Visibility = Visibility.Hidden;
+            frontCard.Visibility = Visibility.Visible;
+
+            DoubleAnimation flipDoubleAnimation = new DoubleAnimation (-1, 1, TimeSpan.FromMilliseconds (1000));
+            flipDoubleAnimation.EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseOut };
+
+            var scale = new ScaleTransform ();
+            frontCard.RenderTransform = scale;
+            scale.BeginAnimation (ScaleTransform.ScaleXProperty, flipDoubleAnimation);
+        }
+        public void BackAnimation()
+        {
+            frontCard.Visibility = Visibility.Hidden;
+            backCard.Visibility = Visibility.Visible;
+
+            DoubleAnimation flipDoubleAnimation = new DoubleAnimation (-1, 1, TimeSpan.FromMilliseconds (1000));
+            flipDoubleAnimation.EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseOut };
+
+            var scale = new ScaleTransform ();
+            backCard.RenderTransform = scale;
+            scale.BeginAnimation (ScaleTransform.ScaleXProperty, flipDoubleAnimation);
+        }
+    }
+}
